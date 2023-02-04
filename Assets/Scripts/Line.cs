45838364e0fcc99d88a3c0eef0c0049718ab7676
BigBigTree4 <sheenaghwu2@outlook.com>
2023-02-04 13:35:07 -0800

    copy the tutorial and test

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public Rigidbody2D rigidbody;
+
+    [HideInInspector] public List<Vector2> points = new List<Vector2>();
+    [HideInInspector] public int pointsCount = 0;
+
+    float pointsMinDistance = 0.1f;
+
+    public void AddPoint(Vector2 newPoint)
+    {
+        if (pointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance)
+            return;
+
+        points.Add(newPoint);
+        pointsCount++;
+
+        //line renderer
+        lineRenderer.positionCount = pointsCount;
+        lineRenderer.SetPosition(pointsCount - 1, newPoint);
+
+        //edge collider
+        if (pointsCount > 1)
+        {
+            edgeCollider.points = points.ToArray();
+        }
+    }
+
+    public Vector2 GetLastPoint()
+    {
+        return (Vector2)lineRenderer.GetPosition(pointsCount - 1);
+    }
+
+    public void UsePhysics(bool usePhysics)
+    {
+        rigidbody.isKinematic = !usePhysics;
+    }
+
+    public void SetLineColor(Gradient colorGradient)
+    {
+        lineRenderer.colorGradient = colorGradient;
+    }
+
+    public void SetPointsMinDistance(float distance)
+    {
+        pointsMinDistance = distance;
+    }
+
+    public void SetLineWidth(float width)
+    {
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+
+        edgeCollider.edgeRadius = width / 2f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
