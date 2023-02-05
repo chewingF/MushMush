051a8ffa62f2c4b981f0d69dfefeca0c3f9d6387
chewingF <379929495@qq.com>
2023-02-04 16:16:28 -0800

    start to build structure

---
+++ Assets/Scripts/RootsManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootsManager : MonoBehaviour
+{
+    // GrowSys
+    // RenderSys
+    // BranchSys
+
+    private Root _mainRoot;
+    private List<Root> _otherRoots = new List<Root>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
