c2a1b93ddf62a4e69a75a0121252531fbeedeff1
chewingF <379929495@qq.com>
2023-02-05 13:53:18 -0800

    a lot of bugs yeah!

---
+++ Assets/Scripts/NavigatorUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class NavigatorUI : MonoBehaviour
+{
+    public GameObject target;
+    public Animator ac;
+    public float secToNavOnce = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ac = this.GetComponent<Animator>();
+    }
+
+    private float timer = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!target){
+            return;
+        }
+        this.transform.LookAt(target.transform.position);
+        this.transform.position = RootsManager.MainRoot().GetLastPoint();
+
+        this.timer += Time.deltaTime;
+        if (this.timer < this.secToNavOnce){
+            return;
+        }
+        if (ac){
+            ac.SetTrigger("FadeOnce");
+        }
+        this.timer = 0;
+        
+    }
+}
